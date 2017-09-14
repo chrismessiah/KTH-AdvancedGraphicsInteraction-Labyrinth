f2456c190fc53e26b59aef4958e2918eceb8ad24
Johan <jthorel@kth.se>
2017-09-14 16:26:36 +0200

    jthorel
    ello world
    
    
    Former-commit-id: b9603a8dcdd10825dbc25ae5df2168e356707e50

---
+++ Assets/Scripts/ControllerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class ControllerMovement : MonoBehaviour
+{
+    public GameObject world;
+    private float moveSpeed = 0.2f;
+    private GameObject player;
+    //player is the camerarig
+    SteamVR_Controller.Device device;
+    SteamVR_TrackedObject controller;
+
+    Vector2 touchpad;
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<SteamVR_TrackedObject>();
+        player = transform.parent.parent.gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)controller.index);
+        //If finger is on touchpad
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            //Read the touchpad values
+            touchpad = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+
+            world.transform.Rotate(0, 0, -1*touchpad.y*moveSpeed);
+            //player.transform.position = new Vector3(player.transform.position.x + (-touchpad.y / 100f), player.transform.position.y, player.transform.position.z + (touchpad.x / 100f));
+            world.transform.Translate(0,0, 0.1f * touchpad.x * moveSpeed);
+        }
+    }
+}
