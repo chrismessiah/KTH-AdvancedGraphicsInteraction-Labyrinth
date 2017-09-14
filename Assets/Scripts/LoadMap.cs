166ff170d116ff22f17b5dc437aedb4d6c1ab359
Christian Abdelmassih <chrabd@kth.se>
2017-09-14 10:15:19 +0200

    Add script which creates a wall
    Former-commit-id: fb2f2b8949cb3d894855131ce2555f6dcfb9d482

---
+++ Assets/Scripts/LoadMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMap : MonoBehaviour {
+
+	void Start () {;
+		CreateWall();
+	}
+
+	void Update () {
+
+	}
+
+	void CreateWall() {
+		// future inputs
+		Vector3 position = new Vector3(1,0,-20);
+		Vector3 scale = new Vector3(10,2,40);
+		//Quaternion rotation = Quaternion.Euler(0, 0, 90); // rotate 90 deg around Z-axis
+
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = position;
+		cube.transform.localScale = scale;
+		//cube.transform.rotation = rotation;
+	}
+}
