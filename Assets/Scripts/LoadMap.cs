8646ce3235e940fdd7c8c85a8ce4524e896a7178
Christian Abdelmassih <chrabd@kth.se>
2017-09-14 18:18:35 +0200

    Add script to generate maze from image file
    Former-commit-id: 191ee6f51b8411ac9b3ed14e1e5c44d2b24b0480

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 public class LoadMap : MonoBehaviour {
 
 	void Start () {;
-		Vector3 position, scale;
-
-		position = new Vector3(1,5,-20);
-		scale = new Vector3(1,10,20);
-		CreateWall(position, scale);
-
-		position = new Vector3(1,5,-20);
-		scale = new Vector3(20,10,1);
-		CreateWall(position, scale);
+		LoadMaze("maze", 300, 100, 0, -300);
 	}
 
 	void Update () {
 
 	}
 
-	void CreateWall(Vector3 position, Vector3 scale) {
-		//Quaternion rotation = Quaternion.Euler(0, 0, 90); // future input, rotate 90 deg around Z-axis
+	// make sure the image settings under Advanced are
+	// 	Read/Write: 		Yes
+	// 	Non Power of 2: 	None
+	// Also make sure that the image is placed in the Resources-folder
+	void LoadMaze(string path, int height, int width, int offsetX, int offsetZ) {
+		Texture2D texture = Resources.Load(path, typeof(Texture2D)) as Texture2D;
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < height; z++) {
+				int num = (int)Mathf.Round(texture.GetPixel(x, z).b);
+				if (num == 0) {
+					Vector3 position = new Vector3(x+offsetX, 3, z+offsetZ);
+					CreateWall(position);
+				}
+			}
+		}
 
+	}
+
+	GameObject CreateWall(Vector3 position) {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = position;
+		cube.transform.localScale = new Vector3(1, 6, 1);
+		return cube;
+	}
+
+	GameObject CreateWall(Vector3 position, Vector3 scale) {
 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		cube.transform.position = position;
 		cube.transform.localScale = scale;
-		//cube.transform.rotation = rotation;
+		return cube;
+	}
+
+	GameObject CreateWall(Vector3 position, Vector3 scale, Quaternion rotation) {
+		GameObject cube = CreateWall(position, scale);
+		cube.transform.rotation = rotation;
+		return cube;
 	}
 }
