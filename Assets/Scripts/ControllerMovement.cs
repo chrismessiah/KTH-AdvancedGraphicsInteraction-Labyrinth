c174261406f0e38996d6a3c307a9fb5aabe65a20
jthorel <jthorel@kth.se>
2017-09-15 11:40:18 +0200

    new control test

--- Assets/Scripts/ControllerMovement.cs
+++ Assets/Scripts/ControllerMovement.cs
@@ -8,14 +8,21 @@ public class ControllerMovement : MonoBehaviour
     private float moveSpeed = 0.2f;
     private GameObject player;
     //player is the camerarig
-    SteamVR_Controller.Device device;
+    SteamVR_Controller.Device device 
+    {
+        get { return SteamVR_Controller.Input((int)controller.index); }
+    }
     SteamVR_TrackedObject controller;
-
     Vector2 touchpad;
+
+
+    void Awake(){
+        controller = gameObject.GetComponent<SteamVR_TrackedObject>();
+    }
 
     void Start()
     {
-        controller = gameObject.GetComponent<SteamVR_TrackedObject>();
+        
         player = transform.parent.parent.gameObject;
 
     }
@@ -23,7 +30,7 @@ public class ControllerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        device = SteamVR_Controller.Input((int)controller.index);
+        //device = SteamVR_Controller.Input((int)controller.index);
         //If finger is on touchpad
         if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
         {
@@ -33,6 +40,8 @@ public class ControllerMovement : MonoBehaviour
             world.transform.Rotate(0, 0, -1*touchpad.y*moveSpeed);
             //player.transform.position = new Vector3(player.transform.position.x + (-touchpad.y / 100f), player.transform.position.y, player.transform.position.z + (touchpad.x / 100f));
             world.transform.Translate(0,0, 0.1f * touchpad.x * moveSpeed);
+            float yRot = device.transform.rot.y;
+            print(transform.forward);
         }
     }
 }
