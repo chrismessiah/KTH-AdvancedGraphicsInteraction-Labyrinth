618d99b2efab5bb090a0f26cd6f9d982d7b84a69
Johan <jthorel@kth.se>
2017-09-19 15:06:24 +0200

    adasdf

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LoadMap : MonoBehaviour {
 
 	void Start () {;
-		LoadMaze("maze", 300, 100, -295, 0);
+		LoadMaze("maze", 300, 100, 0, -300);
 	}
 
 	void Update () {
@@ -17,19 +17,14 @@ public class LoadMap : MonoBehaviour {
 	// 	Non Power of 2: 	None
 	// Also make sure that the image is placed in the Resources-folder
 	void LoadMaze(string path, int height, int width, int offsetX, int offsetZ) {
-		
 		Texture2D texture = Resources.Load(path, typeof(Texture2D)) as Texture2D;
-
-		int angle;
-		for (int x = 0; x < height; x++) {
-			angle = 360;
-			for (int z = 0; z < width; z++) {
-				int num = (int)Mathf.Round(texture.GetPixel(z, x).b);
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < height; z++) {
+				int num = (int)Mathf.Round(texture.GetPixel(x, z).b);
 				if (num == 0) {
 					Vector3 position = new Vector3(x+offsetX, 3, z+offsetZ);
-					CreateWall(position, angle);
+					CreateWall(position);
 				}
-			angle -= 5;
 			}
 		}
 
@@ -42,16 +37,16 @@ public class LoadMap : MonoBehaviour {
 		return cube;
 	}
 
-	GameObject CreateWall(Vector3 position, int angle) {
-		GameObject cube = CreateWall(position);
-		cube.transform.rotation = Quaternion.Euler(angle, 0, 0);
+	GameObject CreateWall(Vector3 position, Vector3 scale) {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = position;
+		cube.transform.localScale = scale;
 		return cube;
 	}
 
-	GameObject CreateWall(Vector3 position, int angle, Vector3 scale) {
-		GameObject cube = CreateWall(position);
-		cube.transform.localScale = scale;
-		cube.transform.rotation = Quaternion.Euler(0, 0, 0);
+	GameObject CreateWall(Vector3 position, Vector3 scale, Quaternion rotation) {
+		GameObject cube = CreateWall(position, scale);
+		cube.transform.rotation = rotation;
 		return cube;
 	}
 }
