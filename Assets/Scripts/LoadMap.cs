1a5dac14f24f5bc0d35bcaa578652b56ed9862b8
Christian Abdelmassih <chrabd@kth.se>
2017-09-25 11:21:48 +0200

    add conversion to radians and add debug maps

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -7,7 +7,7 @@ public class LoadMap : MonoBehaviour {
 	Texture2D texture;
 
 	void Start () {;
-		texture = Resources.Load("maze99", typeof(Texture2D)) as Texture2D;
+		texture = Resources.Load("maps/maze99", typeof(Texture2D)) as Texture2D;
 		wallElementTemplate = Resources.Load("wall-block-perfect", typeof(GameObject)) as GameObject;
 		//shader = Resources.Load("Standard", typeof(Shader)) as Shader;
 
@@ -26,18 +26,19 @@ public class LoadMap : MonoBehaviour {
 	void LoadMaze(int length, int width, int offsetX, int offsetZ) {
 
 		const int R = 99;
-		const float angleStep = 360/360; // just to clarify that the input image is expected to be of length 360
+		const float angleStep = 1.0f; // just to clarify that the input image is expected to be of length 360
 
-		float x, y, z, theta = 0f;
+		float x, y, z, theta = 0f, thetaRadians;
 
 		bool flatMaze = false;
 
 		GameObject wallElement;
 		Vector3 position;
+		Vector3 scale = new Vector3 (2.5f, 1, 2); // depth, width, height
 
-		for (int p1 = 0; p1 < width; p1++) {
+		for (int p1 = 0; p1 < width; p1++) { // loop over short edge
 			theta = 0f;
-			for (int p2 = 0; p2 < length; p2++) {
+			for (int p2 = 0; p2 < length; p2++) { // loop over long edge
 				bool isWall = ((int)Mathf.Round(texture.GetPixel(p1, p2).b)) == 0;
 				if (isWall) {
 
@@ -47,12 +48,13 @@ public class LoadMap : MonoBehaviour {
 						y = 0;
 						z = p2;
 					} else {
-						z = R * Mathf.Cos (theta);
-						y = R * Mathf.Sin (theta);
+						thetaRadians = Mathf.Deg2Rad * theta;
+						z = R * Mathf.Cos (thetaRadians);
+						y = R * Mathf.Sin (thetaRadians);
 					}
 
 					position = new Vector3(x, y, z);
-					wallElement = CreateWall(position);
+					wallElement = CreateWall(position, scale);
 
 					if (!flatMaze) {
 						RotateWall(wallElement);
@@ -78,11 +80,9 @@ public class LoadMap : MonoBehaviour {
 	GameObject CreateWall(Vector3 position) {
 		GameObject wall = Instantiate(wallElementTemplate) as GameObject;
 		wall.AddComponent<MeshCollider>();
-
 		//GetComponent<MeshRenderer>().material = new Material(shader);
-
 		wall.transform.position = position;
-		wall.transform.localScale = new Vector3(2, 0.5f, 2); // depth, width, height
+		wall.transform.localScale = new Vector3(1, 1, 2);
 		return wall;
 	}
 
