125a2cd118734fcd59d9446aedee415640907420
Christian Abdelmassih <chrabd@kth.se>
2017-09-27 14:34:19 +0200

    Add score HUD

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+	Text text;
+	Vector3 startPoint;
+	int maxScore = 0;
+
+	void Awake () {
+		text = GetComponent<Text>();
+		text.text = "  Score: 0";
+	}
+
+	void Start() {
+		startPoint = GameObject.Find("MouseKeyboardPlayer").transform.position;
+	}
+
+	void Update () {
+		Vector3 currentPoint = GameObject.Find("MouseKeyboardPlayer").transform.position;
+		Vector3 distance = currentPoint - startPoint;
+		int currentScore = (int)Mathf.Round(distance.magnitude);
+		if (maxScore < currentScore) {
+			maxScore = currentScore;
+			text.text = "  Score: " + maxScore;
+		}
+	}
+}
