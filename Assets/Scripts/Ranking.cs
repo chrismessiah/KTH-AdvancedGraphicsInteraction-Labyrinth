535db63543f547cc6806026189150833b9cafb8d
Jun Zhang <junzh@kth.se>
2017-10-26 01:00:07 +0200

    writting&reading rank file &sorting

---
+++ Assets/Scripts/Ranking.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Reflection;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+public class Ranking : MonoBehaviour {
+
+	Text text;
+	public GameObject rank;
+	public InputField username;
+
+
+	void Awake () {
+		text = GetComponent<Text>();
+		text.text = "Ending";
+	}
+		
+	// Use this for initialization
+	void Start () {
+//		rank.SetActive (false);
+//		rank = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (TimeScore.instance.isEnded) {
+			rank.SetActive (false);
+			text.text = "Ending";
+		}
+	}
+
+	public void showRanking(){
+		rank.SetActive (false);
+	}
+
+
+	public string resultToJson(string name, int time)
+	{
+		Record record = new Record (name, time);
+		return JsonUtility.ToJson(record);
+	}
+
+	void saveRank(string records)
+	{
+		FileInfo f = new FileInfo(Application.dataPath + "/Resources/rank.txt");
+		StreamWriter writer= null;
+		if (f.Exists)
+		{
+			writer = f.AppendText();
+		}
+		else
+		{
+			writer = f.CreateText();
+		}
+		writer.WriteLine(records);
+		writer.Close();
+	}
+
+	void readRank(){
+		FileStream f = new FileStream(Application.dataPath + "/Resources/rank.txt", FileMode.Open);
+		Record temp = new Record ();
+		List<Record> records = new List<Record>();	
+		if (f.CanRead){
+			StreamReader reader = new StreamReader(f);
+			string str;
+			while ((str = reader.ReadLine ()) != null) {
+				temp = JsonUtility.FromJson<Record> (str);
+				records.Add (temp);
+			}
+		}
+		List<Record> SortedList = records.OrderBy(r=>r.time).ToList();
+	}
+}
+
+class Record{
+	public string name;
+	public int time;
+	public Record(){
+	}
+	public Record(string n, int t){
+		name = n;
+		time = t;
+	}
+}
---
+++ Assets/Scripts/TimeScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScore : MonoBehaviour {
+	Text text;
+	public float timepassing = 0.0f;
+	public static TimeScore instance;
+	public bool isEnded = false;
+
+	GameController gameController {
+		get { return GameController.instance; }
+	}
+
+	void Awake () {
+		text = GetComponent<Text>();
+		text.text = "  Time Passing: 0 sec";
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = this;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isEnded) {
+			timepassing += Time.deltaTime;
+			text.text = "  Time Passing: " + Mathf.Round (timepassing) + " sec";
+		}
+	}
+
+	public void StopTimer(){
+		isEnded = true;
+//		Ranking.instance.showRanking ();
+	}
+}
