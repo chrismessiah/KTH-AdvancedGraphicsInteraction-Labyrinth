c68815dc1f246be0974507392572f479671f6dc3
adelinatahiri <tahiri@kth.se>
2017-10-26 15:12:18 +0200

    Start-timer

---
+++ Assets/TImerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TImerScript : MonoBehaviour {
+
+	Text text;
+	float timeLeft = 3f;
+
+	void Awake () {
+		text = GetComponent<Text>();
+	}
+
+
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if (timeLeft > 0) {
+
+			text.text = Mathf.Round (timeLeft).ToString ("f0");
+		}
+		if (timeLeft < 0) {
+			text.text = "Start";
+		}
+
+		if (timeLeft < -2) {
+			Debug.Log (timeLeft);
+			gameObject.SetActive (false);
+		}
+	}
+
+}
+
